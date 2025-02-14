405b47d86feb182815ea3e0da45fc46af5fd4a81
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-14 13:07:54 +0100

    Improved training step and action selection

--- src/agent_class.py
+++ src/agent_class.py
@@ -15,9 +15,9 @@ class DQN_Agent:
                  epsilon_decay: float=0.995, epsilon_min: float=0.01,
                  buffer_maxlen: int=2000, batch_size: int=32):
         
-        self.state_size = state_size
-        self.action_size = action_size
-        self.target_model = model.clone()
+        self.state_size: int = state_size
+        self.action_size: int = action_size
+        self.target_model: nn.Module = model.clone()
         self.replay_buffer = deque(maxlen=buffer_maxlen)
         
         self.model = model #The nn model
@@ -39,18 +39,8 @@ class DQN_Agent:
                         reward, next_state: th.tensor, done: bool):
         """Store the experience to the replay buffer as (state, action, reward, next_state, done)"""
         self.replay_buffer.append((state, action, reward, next_state, done))
-        
-    def choose_action_(self, state):
-        "Choose an action based on the epsilon-greedy policy"
-        if np.random.rand() <= self.epsilon:
-            # return a random number between 0 and 4
-            return np.random.choice(self.action_size)
-        
-        # Predict the Q values of the state
-        with th.no_grad():
-            q_values = self.model(state)
-        return th.argmax(q_values).item()
     
+    # Exploration step
     def train_step(self):
         """Train the model using the experiences from the replay buffer"""
         
@@ -66,22 +56,44 @@ class DQN_Agent:
         next_states = th.FloatTensor([exp[3] for exp in minibatch])
         dones = th.BoolTensor([exp[4] for exp in minibatch])
         
-        # Compute the Q values of the states
-        current_q_values = self.model(states)
-
-        # Compute the Q values of the next states using the target network
-        next_q_values = self.target_model(next_states)
-        
-        # Compute the target Q values
-        target = rewards + self.gamma * th.max(next_q_values, dim=1).values * (1 - dones)
+        loss = self._compute_loss(states, actions, rewards, next_states, dones)
         
         # Compute the loss
         self.optimizer.zero_grad()
-        self.loss_function.backward()
+        loss.backward()
+        self.optimizer.step()
         
         # Decay epsilon
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+        
+    def _compute_loss(self, states, actions, rewards, next_states, dones):
+        """Compute the loss"""
+        # Q pred
+        current_q_values = self.model(states)
+        
+        # Q target
+        with th.no_grad():
+            next_q_values = self.target_model(next_states).max(dim=1)[0]
+            target_q_values = rewards + self.gamma * next_q_values * ~dones
+            loss = self.loss_function(current_q_values, target_q_values)
+        return loss
+    
+    def choose_action_(self, state: th.tensor, epsilon: float):
+        """Choose an exploration or exploitation based on the epsilon-greedy policy
+        and track if the action is exploration or exploitation"""
+        if np.random.rand() <= epsilon:
+            # Exploration: return a random number between 0 and 4
+            # return a random number between 0 and 4
+            action = np.random.choice(self.action_size)
+            is_exploration = True
+        else:
+            # Exploitation: return the action with the highest Q value
+            with th.no_grad():
+                q_values = self.model(state)
+            action = th.argmax(q_values).item()
+            is_exploration = False
+        return action, is_exploration
             
     def update_target_model(self):
         """Update the target model"""
