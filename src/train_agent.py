595b768c7cac8fb7f6ebb46429e029ec9fd92655
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-16 13:50:19 +0100

    Bug fix and added save option

--- src/train_agent.py
+++ src/train_agent.py
@@ -1,4 +1,5 @@
 import torch as th
+import numpy as np
 import argparse
 from pathlib import Path
 import json
@@ -18,7 +19,7 @@ paths_file_path = args.paths
 
 with open(paths_file_path) as f:
     paths = json.load(f)
-final_score_path = paths["final_score_path"]
+final_score_path = paths["final_result_path"]
 
 params_file_path = args.params
 config = load_config(params_file_path, ["agent", "CNN_model", "training"])
@@ -62,42 +63,74 @@ locals().update(config["agent"])
 print("Agent initialised\n")
 
 # Initialise the game environment
-game_env = Game2048_env(params_file_path, maxN_emptycells_reward, grid_size)
+game_env = Game2048_env(size=grid_size)
 print("Game environment initialised\n")
 
 final_scores = []
+max_value_reached = []
+reward_function = maxN_emptycells_reward
 
 print("Training the agent\n")
 for episode in range(n_episodes):
-    print(f"Episode {episode + 1}/{n_episodes}")
+    if (episode + 1) % print_every == 0:
+        print(f"Episode {episode + 1}/{n_episodes}")
     state = game_env.reset() # Put the grid in the initial state
+    state = th.tensor(state, dtype=th.float32) # Convert the state to a tensor
     done = False # Initialize the done variable
     total_reward = 0 # Initialize the total reward
     is_action_exploratory = [] # Initialize the list of exploratory actions
     
     while not done:
         # Choose an action
-        action, is_exploratory = agent.choose_action(state, training=True)
+        action, is_exploratory = agent.choose_action(state.unsqueeze(0).unsqueeze(0), training=True)
         
         # Take the action and observe the next state and reward
-        next_state, reward, done = game_env.step(action)
+        next_state, done = game_env.step(state.numpy(), action)
         
+        reward = reward_function(state.numpy(), next_state, done, params_file_path)
+        
+        next_state = th.tensor(next_state, dtype=th.float32) # Convert the next state to a tensor
         # Store the experience in the replay buffer
-        agent.store_to_buffer(state, action, reward, next_state, done)
+        agent.store_to_buffer(state.unsqueeze(0), action, reward, next_state.unsqueeze(0), done)
         
-        # Update the state
-        state = next_state
         total_reward += reward # Update the total reward
+        
+        state = next_state # Update the state
         is_action_exploratory.append(is_exploratory) # Store the exploratory action
     
     # Store the final score
     final_scores.append(total_reward)
+    max_value_reached.append(np.max(state.numpy()))
         
     # Train the model
-    agent.train_step()
-    print(f"Total reward: {total_reward}\n")
+    agent.train_step(episode)
+    if (episode + 1) % print_every == 0:
+        print(f"Total reward: {total_reward}\n")
+    
+
+with open(params_file_path, 'r') as f:
+    params = json.load(f)
+
+json_str = json.dumps(params, indent=4)[1: -1]
+
+i = 0
     
 # Save the final scores to a file
-with open('final_scores.txt', 'w') as f:
-    for score in final_scores:
-        f.write(f"{score}\n")
+with open(final_score_path, 'w') as f:
+    f.write("Final score:\n")
+    for i, score in enumerate(final_scores):
+        f.write(f"{score}")
+        if i < len(final_scores) - 1:
+            f.write("\t")
+    f.write("\n\n")
+    f.write("Max value reached:\n")
+    for i, value in enumerate(max_value_reached):
+        f.write(f"{value}")
+        if i < len(max_value_reached) - 1:
+            f.write("\t")
+    f.write("\n\n")
+    f.write("Parameters:")
+    f.write(json_str)
+    
+print(f"Final scores saved to {final_score_path}\n")
+        
