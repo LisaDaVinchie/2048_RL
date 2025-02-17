5650425169826c9ac303e7cb886c88fcbdf7db1b
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-17 14:45:54 +0100

    Added epsilon

--- src/plot_results.py
+++ src/plot_results.py
@@ -28,13 +28,13 @@ else:
     file_path, IDX = find_latest_file(final_result_basename, result_file_ext)
 
 print(f"Reading data from: {file_path}")
-final_scores, max_values = read_data(file_path)
+final_scores, max_values, epsilons = read_data(file_path)
 
 print(f"Final scores shape: {len(final_scores)}")
 print(f"Max values shape: {len(max_values)}")
 
 # Plot the data
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
 axs[0].plot(final_scores)
 axs[0].set_title("Final Scores")
@@ -45,6 +45,11 @@ axs[1].plot(max_values)
 axs[1].set_title("Max Value reached")
 axs[1].set_xlabel("Episode")
 axs[1].set_ylabel("Value")
+
+axs[2].plot(epsilons)
+axs[2].set_title("Epsilon")
+axs[2].set_xlabel("Episode")
+axs[2].set_ylabel("Value")
 
 plt.tight_layout()
 
