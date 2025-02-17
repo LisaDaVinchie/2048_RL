503ab6e0486671a5932f6fba55c77b6020b4fb38
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-17 14:46:50 +0100

    Added game visualization functions

---
+++ src/utils/visualize_game.py
@@ -0,0 +1,19 @@
+
+def print_grid(grid):
+    print("\n")
+    print("-----------------")
+    for row in grid:
+        print(row)
+    print("-----------------")
+    print("\n")
+
+def save_grid_to_file(grid, file_path):
+    with open(file_path, 'a') as f:
+        f.write("\n")
+        f.write("-----------------")
+        for row in grid:
+            f.write("\n")
+            f.write(" ".join(map(str, row)))
+        
+        f.write("\n-----------------")
+        f.write("\n")
