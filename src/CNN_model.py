376f2bed5988e340f061a07f60528be2925dfbd6
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-17 14:45:43 +0100

    Added layer

--- src/CNN_model.py
+++ src/CNN_model.py
@@ -8,13 +8,15 @@ class CNN_model(nn.Module):
     def __init__(self, grid_size: int, action_size: int,
                  middle_channels: Tuple[int, int, int] = (64, 64, 64),
                  kernel_sizes: Tuple[int, int, int] = (3, 3, 3),
-                 padding: Tuple[int, int, int] = (1, 1, 1)):
+                 padding: Tuple[int, int, int] = (1, 1, 1), softmax: bool = True):
         super(CNN_model, self).__init__()
         
         final_grid_size = grid_size - sum(kernel_sizes) + len(kernel_sizes) + 2 * sum(padding)
         
         if final_grid_size < 1:
             raise ValueError("The kernel sizes are too large for the grid size")
+        
+        self.softmax = softmax
         
         strides = [1 for _ in range(len(kernel_sizes))]
         in_channels = 1
@@ -35,16 +37,20 @@ class CNN_model(nn.Module):
                                stride=strides[2],
                                padding=padding[2])
         
-        self.fc1 = nn.Linear(middle_channels[2] * final_grid_size * final_grid_size, action_size)
+        self.fc1 = nn.Linear(middle_channels[2] * final_grid_size * final_grid_size, 128)
+        self.fc2 = nn.Linear(128, action_size)
         
         self.activation = nn.ReLU()
         
-    def forward(self, x: th.Tensor, softmax: bool = True) -> th.Tensor:
+    def forward(self, x: th.Tensor) -> th.Tensor:
+        x = th.log2(x + 1)/11
         x = self.activation(self.conv1(x))
         x = self.activation(self.conv2(x))
+        x = self.activation(self.conv3(x))
         x = x.view(x.size(0), -1)
         x = self.activation(self.fc1(x))
-        if softmax:
+        x = self.fc2(x)
+        if self.softmax:
             return F.softmax(x, dim=1)
         else:
             return x
