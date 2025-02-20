d948fa1afc6ff18d2726ff034ffd9584c3826511
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-20 10:41:35 +0100

    Small changes

--- test/CNN_model_test.py
+++ test/CNN_model_test.py
@@ -10,7 +10,7 @@ from tempfile import NamedTemporaryFile
 # Add the parent directory to the path so that we can import the game module
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
-from src.CNN_model import CNN_model
+from src.models import CNN_model
 
 class TestCNNModel(unittest.TestCase):
     
@@ -18,23 +18,15 @@ class TestCNNModel(unittest.TestCase):
         """Create a temporary JSON file with reward parameters."""
         self.model_params = {
             "CNN_model": {
-                "middle_channels": [64, 64, 64],
+                "middle_channels": [64, 64, 64, 128],
                 "kernel_sizes": [3, 4, 3],
                 "padding": [1, 1, 1],
                 "softmax": False
             },
             "agent":{
-                "state_size": 16,
                 "action_size": 4,
                 "grid_size": 4,
-                "n_channels": 11,
-                "gamma": 0.95,
-                "epsilon": 1.0,
-                "epsilon_decay": 0.00001,
-                "epsilon_min": 0.3,
-                "buffer_maxlen": 2000,
-                "batch_size": 64,
-                "target_update_freq": 10
+                "n_channels": 11
             }      
         }
         
@@ -48,7 +40,10 @@ class TestCNNModel(unittest.TestCase):
         self.model = CNN_model(params_path=self.params_path)
 
         self.batch_size = 10
-        self.input_tensor = th.rand((self.batch_size, 11, 4, 4))
+        possible_values = [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+        self.input_tensor = th.zeros((self.batch_size,1, self.model.grid_size, self.model.grid_size))
+        for i in range(self.batch_size):
+            self.input_tensor[i, 0, :, :] = th.tensor([[possible_values[i % len(possible_values)] for _ in range(self.model.grid_size)] for _ in range(self.model.grid_size)])
         
     
     def tearDown(self):
@@ -63,7 +58,7 @@ class TestCNNModel(unittest.TestCase):
         self.assertEqual(self.model.grid_size, 4)
         self.assertEqual(self.model.action_size, 4)
         self.assertEqual(self.model.n_channels, 11)
-        self.assertEqual(self.model.middle_channels, [64, 64, 64])
+        self.assertEqual(self.model.middle_channels, [64, 64, 64, 128])
         self.assertEqual(self.model.kernel_sizes, [3, 4, 3])
         self.assertEqual(self.model.padding, [1, 1, 1])
         self.assertFalse(self.model.softmax)
