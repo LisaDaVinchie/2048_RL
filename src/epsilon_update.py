9d96d2d66890d0b52d9ceb0e3fa8e68be04d24c9
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-21 10:18:05 +0100

    Epsilon update policies

--- src/epsilon_update.py
+++ src/epsilon_update.py
@@ -2,25 +2,26 @@ import math
 from pathlib import Path
 from utils.import_params_json import load_config
 
-def exponential(epsilon: float, episode: int, params_path: Path, epsilon_decay: float = None, epsilon_min: float = None):
-    agent_params = load_config(params_path, ["agent"]).get("agent", {})
-    eps_decay = epsilon_decay if epsilon_decay is not None else agent_params.get("epsilon_decay", 0.995)
-    eps_min = epsilon_min if epsilon_min is not None else agent_params.get("epsilon_min", 0.01)
-    
-    if epsilon > eps_min:
-        new_epsilon = eps_min + (epsilon - eps_min) * math.exp(-eps_decay * episode)
-    
-    return new_epsilon
+class exponential():
+    def __init__(self, params_path: Path, epsilon_decay: float = None, epsilon_min: float = None):
+        agent_params = load_config(params_path, ["agent"]).get("agent", {})
+        self.epsilon_decay = epsilon_decay if epsilon_decay is not None else agent_params.get("epsilon_decay", 0.995)
+        self.epsilon_min = epsilon_min if epsilon_min is not None else agent_params.get("epsilon_min", 0.01)
         
-def multiply(epsilon: float, episode: int, params_path: Path, epsilon_decay: float = None, epsilon_min: float = None, explore_for: int = None):
-    agent_params = load_config(params_path, ["agent"]).get("agent", {})
-    eps_decay = epsilon_decay if epsilon_decay is not None else agent_params.get("epsilon_decay", 0.995)
-    eps_min = epsilon_min if epsilon_min is not None else agent_params.get("epsilon_min", 0.01)
-    exp_for = explore_for if explore_for is not None else agent_params.get("explore_for", 5000)
+    def update(self, epsilon: float, episode: int):
+        
+        if epsilon > self.epsilon_min:
+            new_epsilon = self.epsilon_min + (epsilon - self.epsilon_min) * math.exp(-self.epsilon_decay * episode)
+        
+        return new_epsilon
     
-    if episode % exp_for == 0:
-            new_epsilon = new_epsilon * 2
-    else:
-        new_epsilon = max(eps_min, epsilon * eps_decay)
+class multiply():
+    def __init__(self, params_path: Path, epsilon_decay: float = None, epsilon_min: float = None, explore_for: int = None):
+        agent_params = load_config(params_path, ["agent"]).get("agent", {})
+        self.epsilon_decay = epsilon_decay if epsilon_decay is not None else agent_params.get("epsilon_decay", 0.995)
+        self.epsilon_min = epsilon_min if epsilon_min is not None else agent_params.get("epsilon_min", 0.01)
+        self.explore_for = explore_for if explore_for is not None else agent_params.get("explore_for", 500)
     
-    return new_epsilon 
+    def update(self, epsilon: float, episode: int):
+        new_epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
+        return new_epsilon
