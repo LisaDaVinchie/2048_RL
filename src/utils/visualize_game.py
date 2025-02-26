20e07050684f65c3eb0f8c58344cb0223ef3e56e
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-26 10:43:59 +0100

    Small changes

--- src/utils/visualize_game.py
+++ src/utils/visualize_game.py
@@ -1,19 +1,19 @@
 
 def print_grid(grid):
     print("\n")
-    print("-----------------")
+    print("------------------------------------")
     for row in grid:
-        print(row)
-    print("-----------------")
+        print("\t".join(map(str, row)))
+    print("------------------------------------")
     print("\n")
 
 def save_grid_to_file(grid, file_path):
     with open(file_path, 'a') as f:
         f.write("\n")
-        f.write("-----------------")
+        f.write("------------------------------------")
         for row in grid:
             f.write("\n")
             f.write(" ".join(map(str, row)))
         
-        f.write("\n-----------------")
+        f.write("\n------------------------------------")
         f.write("\n")
