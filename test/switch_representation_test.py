2aaaa6ee19338ebe5f68fbd38b20b4b70091625e
LisaDaVinchie <lisadavinchie@gmail.com>
2025-02-28 12:09:38 +0100

    Solved wrong test

--- test/switch_representation_test.py
+++ test/switch_representation_test.py
@@ -8,23 +8,15 @@ from src.utils.representations import to_one_hot, from_one_hot
 
 class Test2048Encoding(unittest.TestCase):
     def setUp(self):
-        self.batch_size = 2
-        self.test_grid = np.array([
-            [[0, 2, 4, 8],
+        self.test_grid = np.array([[0, 2, 4, 8],
              [16, 32, 64, 128],
              [256, 512, 1024, 2048],
-             [0, 0, 2, 4]],
-            [[2, 4, 8, 16],
-             [32, 64, 128, 256],
-             [512, 1024, 2048, 0],
-             [0, 2, 4, 8]]
-        ])  # Shape: [2, 4, 4]
-        
+             [0, 0, 2, 4]])  # Shape: [4, 4]
         self.n_channels = int(np.log2(self.test_grid.max())) + 2
     
     def test_to_one_hot_shape(self):
         one_hot_encoded = to_one_hot(self.test_grid, self.n_channels)
-        expected_shape = (self.batch_size, self.n_channels, 4, 4)
+        expected_shape = (self.n_channels, 4, 4)
         self.assertEqual(one_hot_encoded.shape, expected_shape)
     
     def test_from_one_hot(self):
