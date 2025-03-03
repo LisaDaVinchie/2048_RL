7d9b0c2945356a7faffa83ba97a0b2f68af03c58
LisaDaVinchie <lisadavinchie@gmail.com>
2025-03-03 11:05:20 +0100

    Added random game playing

--- src/play_random_game.py
+++ src/play_random_game.py
@@ -3,12 +3,7 @@ import argparse
 from pathlib import Path
 import json
 from time import time
-
-from utils.import_params_json import load_config
-from utils.visualize_game import print_grid
 from game_class import Game2048_env # Import the game class
-# from rewards import maxN_emptycells_reward, original_reward, maxN_emptycells_merge_reward, log2_merge_reward, sum_maxval_reward
-# reward_function = original_reward
 start_time = time()
 
 parser = argparse.ArgumentParser()
@@ -22,7 +17,7 @@ with open(paths_file_path) as f:
     paths = json.load(f)
 results_path = Path(paths["random_result_path"])
 
-if not results_path.exists():
+if not results_path.parent.exists():
     raise FileNotFoundError(f"The path {results_path} does not exist")
 
 with open(params_file_path) as f:
@@ -35,10 +30,10 @@ n_channels = params["agent"]["n_channels"]
 game_env = Game2048_env(size=grid_size)
 
 max_values = []
-total_rewards = []
+final_score = []
 actions = []
 
-n_games = 10
+n_games = 100
 
 i = 0
 while i < n_games:
@@ -70,40 +65,42 @@ while i < n_games:
             break
         state = next_state
         old_reward = reward
+        
     max_values.append(max_value)
-    total_rewards.append(total_reward)
+    final_score.append(total_reward)
     actions.append(game_actions)
     
-    elapsed_time = time() - start_time
-    print(f"Game {i} finished in {elapsed_time} seconds. Total reward: {total_reward}. Max value: {max_value}")
+
+
+elapsed_time = time() - start_time
+with open(results_path, 'w') as f:
+    f.write('Time taken [s]: ')
+    f.write(str(elapsed_time))
+    f.write('\n\n')
     
-    with open(results_path, 'w') as f:
-        f.write('Time taken [s]: ')
-        f.write(str(elapsed_time))
-        f.write('\n\n')
-        
-        f.write("Games played:\n")
-        f.write(str(n_games))
-        f.write("\n\n")
-        
-        f.write("Final score:\n")
-        for i, score in enumerate(total_rewards):
-            f.write(f"{score}")
-            if i < len(total_rewards) - 1:
+    f.write("Games played:\n")
+    f.write(str(n_games))
+    f.write("\n\n")
+    
+    f.write("Final score:\n")
+    for i, score in enumerate(final_score):
+        f.write(f"{score}")
+        if i < len(final_score) - 1:
+            f.write("\t")
+    f.write("\n\n")
+    
+    f.write("Max value reached:\n")
+    for i, value in enumerate(max_values):
+        f.write(f"{value}")
+        if i < len(max_values) - 1:
+            f.write("\t")
+            
+    f.write("\n\n")
+    
+    f.write("Actions:\n")
+    for i, game_actions in enumerate(actions):
+        for j, action in enumerate(game_actions):
+            f.write(f"{action}")
+            if j < len(game_actions) - 1:
                 f.write("\t")
-        f.write("\n\n")
-        
-        f.write("Max value reached:\n")
-        for i, value in enumerate(max_values):
-            f.write(f"{value}")
-            if i < len(max_values) - 1:
-                f.write("\t")
-        f.write("\n\n")
-        
-        f.write("Actions:\n")
-        for i, game_actions in enumerate(actions):
-            for j, action in enumerate(game_actions):
-                f.write(f"{action}")
-                if j < len(game_actions) - 1:
-                    f.write("\t")
-            f.write("\n")
+        f.write("\n")
